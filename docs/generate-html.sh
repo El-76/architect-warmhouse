9e0e9748c1557b7765426255c068b3950dcfe93b
maaaah <maaaah@debian.myguest.virtualbox.org>
2026-07-06 12:41:59 +0300

    Generate html.

--- docs/generate-html.sh
+++ docs/generate-html.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+
+#openapi
+
+for F in `find openapi/ -maxdepth 1 -mindepth 1 -type f -! -name openapi.yaml -printf '%p '`; do
+    D=`basename -s .yaml $F`
+
+    mkdir -p ./openapi/html/${D}/
+
+    npx @openapitools/openapi-generator-cli generate -i $F -g html2 -o ./openapi/html/${D}/
+done
+
+
 #asyncapi
 
 asyncapi generate fromTemplate asyncapi/*.yaml @asyncapi/html-template@0.28.4 --force-write -o asyncapi/html/
 
 
-#openapi
 
-npx @redocly/cli join `find openapi/ -maxdepth 1 -mindepth 1 -type f -! -name openapi.yaml -printf '"%p" '` -o ./openapi/openapi.yaml
-
-npx @openapitools/openapi-generator-cli generate -i openapi/openapi.yaml -g html2 -o ./openapi/html/
